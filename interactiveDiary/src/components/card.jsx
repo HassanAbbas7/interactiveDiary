0c3642c267621e060572d136281567e3be7a7538
HassanAbbas7 <ph015016test1@gmail.com>
2024-03-12 23:37:38 +0500

    fixes

--- interactiveDiary/src/components/card.jsx
+++ interactiveDiary/src/components/card.jsx
@@ -1,16 +1,19 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Draggable from 'react-draggable';
 import Xarrow, {Xwrapper, useXarrow} from "react-xarrows";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/fontawesome-free-solid';
+import { faPlus, faTrash, faLink, faEdit } from '@fortawesome/fontawesome-free-solid';
 import { Link } from 'react-router-dom';
 
 
-const Card = ({ title, content, id, previousQuestionId, isPrevQ, spawnNewCard, layer}) => {
+const Card = ({ title, content, id, handleCardClick, setActiveLinkCard, editCardContent, previousQuestionId, isPrevQ, spawnNewCard, layer, deleteCard}) => {
   const updateXarrow = useXarrow();
 
+  const [isEditable, setIsEditable] = useState(false)
   const isOption = title=="Option"? true: false
   const eligibleForPlus = !(isOption && isPrevQ)
+
+
 
   const handleOnStop=(data, e) => {
     updateXarrow()
@@ -20,28 +23,45 @@ const Card = ({ title, content, id, previousQuestionId, isPrevQ, spawnNewCard, l
     updateXarrow()
   }
 
+
   
     return (
-      <>
+      <div>
         <Draggable onDrag={handleOnStart} onStop={(data, e)=>{handleOnStop(data, e)}}>
   <div className="card-container" style={{ display: 'flex' }}>
 
     <div className="card" id={id}>
-      <div className="header">{title}</div>
-      {content && <div className="content">{content}</div>}
+      <div  onClick={() => handleCardClick(id)} className="header">{title}</div>
+      <div className="content">
+        {isEditable ? <textarea value={content} onChange={(e) => {editCardContent(id, e.target.value, isOption)}}></textarea>: content}
+        </div>
     </div>
 
-    {eligibleForPlus &&
       <div className='icon-container'>
+        <button>
+          <FontAwesomeIcon icon={faTrash} onClick={() => deleteCard(id)} style={{ fontSize: '24px' }}/>
+        </button>
+
+        {eligibleForPlus && <>
         <button onClick={() => spawnNewCard(id, layer, isOption)}>
           <FontAwesomeIcon icon={faPlus} style={{ fontSize: '24px' }} />
         </button>
+        <button>
+          <FontAwesomeIcon onClick={()=>{setActiveLinkCard(id); console.log("set the card link")}} icon={faLink} style={{ fontSize: '24px' }}/>
+        </button>
+        
+        </>
+        }
+      <button>
+          <FontAwesomeIcon onClick={() => setIsEditable(!isEditable)} icon={faEdit} style={{ fontSize: '24px', color: isEditable?'blue':'' }} />
+        </button>
+        
       </div>
-    }
+    
 
   </div>
 </Draggable>
-        </>
+        </div>
     )
   }
   
--- interactiveDiary/src/components/testing.jsx
+++ interactiveDiary/src/components/testing.jsx
@@ -1,89 +1,106 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Card from "./card";
 import { ArcherContainer, ArcherElement } from 'react-archer';
-import Xarrow, {Xwrapper, useXarrow} from "react-xarrows";
+import Xarrow, { Xwrapper, useXarrow } from "react-xarrows";
 import Draggable from "react-draggable";
 
 
-const Testing = ()=>{
-    
+const Testing = () => {
+
   const [newCards, setNewCards] = useState([])
-  const [cards, setCards] = useState([
-    {
+  const [cards, setCards] = useState();
+  const [organisedCards, setOrganisedCards] = useState([])
+  const [activeLinkCard, setActiveLinkCard] = useState(null)
+
+
+
+
+  useEffect(() => {
+    localStorage.setItem('cards', JSON.stringify([
+      {
         id: 1,
         question: 'How was your day?',
-        options:[
-            {
-                id: 2,
-                text: 'Good',
-                nextQuestion: {
-                    id: 3,
-                    question: 'How good?',
-                    options: [
-                        {
-                            id: 4,
-                            text: 'Very Good!',
-                        },
-                        {
-                            id: 5,
-                            text: 'Not so good',
-                        },
-                        {
-                            id: 6,
-                            text: 'Meh',
-                        }
-                    ]
+        options: [
+          {
+            id: 2,
+            text: 'Good',
+            nextQuestion: {
+              id: 3,
+              question: 'How good?',
+              options: [
+                {
+                  id: 4,
+                  text: 'Very Good!',
+                },
+                {
+                  id: 5,
+                  text: 'Not so good',
+                },
+                {
+                  id: 6,
+                  text: 'Meh',
                 }
-            },
-            {
-                id: 7,
-                text: 'Bad',
-                nextQuestion: false
-            },
-            {
-                id: 8,
-                text: 'Neutral',
-                nextQuestion: false
+              ]
             }
+          },
+          {
+            id: 7,
+            text: 'Bad',
+            nextQuestion: false
+          },
+          {
+            id: 8,
+            text: 'Neutral',
+            nextQuestion: false
+          }
         ]
+      }
+    ]))
+  })
+
+
+
+  
+
+
+  useEffect(() => {
+    setCards(JSON.parse(localStorage.getItem('cards')))
+  }, [])
+
+
+  const makeLayers = (obj = {}, layer = 0, prevQuestionId = false) => {
+    var newCard_ = {};
+    if (Object.keys(obj).length === 0 || Array.isArray(obj)) {
+      layer -= 1;
     }
-]);
-  const [organisedCards, setOrganisedCards] = useState([])
+    Object.keys(obj).forEach((key) => {
+      if (typeof obj[key] === 'object') {
+        makeLayers(obj[key], layer + 1, obj.id ? obj.id : prevQuestionId);
+        return;
+      }
+      newCard_[key] = obj[key];
+      newCard_.layer = layer;
+      newCard_.previousQuestion = [prevQuestionId];
+    });
 
-
-const makeLayers = (obj = {}, layer = 0, prevQuestionId = false) => {
-  var newCard_ = {};
-  if (Object.keys(obj).length === 0 || Array.isArray(obj)) {
-    layer -= 1;
-  }
-  Object.keys(obj).forEach((key) => {
-    if (typeof obj[key] === 'object') {
-      makeLayers(obj[key], layer + 1, obj.id ? obj.id : prevQuestionId);
+    if (Object.keys(obj).length === 0 || Array.isArray(obj)) {
       return;
     }
-    newCard_[key] = obj[key];
-    newCard_.layer = layer;
-    newCard_.previousQuestion = prevQuestionId;
-  });
 
-  if (Object.keys(obj).length === 0 || Array.isArray(obj)) {
-    return;
-  }
+    setNewCards((prevCards) => {
+      if (!Array.isArray(prevCards)) {
+        return [newCard_];
+      }
+      return [...prevCards, newCard_];
+    });
+  };
 
-  setNewCards((prevCards) => {
-    if (!Array.isArray(prevCards)) {
-      return [newCard_];
-    }
-    return [...prevCards, newCard_];
-  });
-};
+  useEffect(() => {
+    makeLayers(cards);
+  }, [cards, setNewCards]);
 
-useEffect(() => {
-  makeLayers(cards);
-}, [cards, setNewCards]);
-
-useEffect(() => {
+  useEffect(() => {
     console.log(newCards);
     var temp_ = {};
     for (let card of newCards) {
@@ -95,71 +112,132 @@ useEffect(() => {
     console.log(temp_);
     setOrganisedCards(Object.values(temp_));
   }, [newCards]);
-  const boxStyle = {border: 'grey solid 2px', borderRadius: '10px', padding: '5px'};
+  
+  const boxStyle = { border: 'grey solid 2px', borderRadius: '10px', padding: '5px' };
+
 
   const idInPreviousQuestionIds = (id) => {
     return organisedCards.some((card) => {
       return card.some((card_) => {
-        console.log(card_.previousQuestion, id);
-        return parseInt(card_.previousQuestion) === parseInt(id);
-      });
+        console.log(card_.previousQuestion?.includes(parseInt(id)));
+        return card_.previousQuestion?.includes(parseInt(id));
+      })
     });
+  };
+
+  const editCardContent = (id, content, isOption) => {
+    setNewCards((prevCards) => {
+      return prevCards.map((card) => {
+        if (card.id === id) {
+          console.log("editing: ")
+          console.log(card)
+          if (isOption) {
+            return {
+              ...card,
+              text: content,
+
+            }
+          }
+          else {
+            return {
+              ...card,
+              question: content,
+            }
+          }
+
+        }
+        return card
+      })
+    })
   }
-  
+
+
+
   const spawnNewCard = (parentId, parentLayer, isOption) => {
-    var babyCard = {id: getNewId(), layer: parentLayer + 1, previousQuestion: parentId};
+    var babyCard = { id: getNewId(), layer: parentLayer + 1, previousQuestion: [parentId] };
     if (!isOption) {
       babyCard.text = 'OPTION TEXT'
     }
-    else{
+    else {
       babyCard.question = 'QUESTION TEXT'
     }
     setNewCards((prevCards) => [...prevCards, babyCard]);
   }
 
-  const getNewId=()=>{
+  const deleteCard = (id) => {
+    setNewCards((prevCards) => prevCards.filter((card) => card.id !== id));
+  }
+
+  const getNewId = () => {
     let randomNum = 0;
     let excludedNumbers = newCards.map((card) => card.id);
     do {
-      randomNum = randomNum+1;
+      randomNum = randomNum + 1;
     } while (excludedNumbers.includes(randomNum));
     return randomNum
   }
 
-    return (
-        <html>
- <head>
- </head>
- <body>
-  <p></p>
-  <button onClick={() => console.log(spawnNewCard())}>show new cards</button>
-  <Xwrapper>
-  {/* <Xarrow start={} end={(card_.id).toString()}/> */}
- <div style={{ display: 'flex', height: '100vh', flexDirection: 'row' }}>
-    {organisedCards.map((card_, index) => (
-      
-      <div key={card_.id} style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-    {
-      card_.map((card_, index) => (
-        console.log(card_.prevQuestion),
-        <>
-        {Number.isInteger(card_.previousQuestion)? <Xarrow start={(card_.previousQuestion).toString()} end={(card_.id).toString()}/>: ""}
-        <div style={{display: 'flex', justifyContent: 'space-evenly', width: '100%'}}>
-            
-                <Card id={card_.id} previousQuestionId={card_.previousQuestion} isPrevQ={idInPreviousQuestionIds(card_.id)} title={card_.question? "Question": "Option"} layer={card_.layer} content={card_.text? card_.text: card_.question} spawnNewCard={spawnNewCard}/>
+  const handleCardClick = (id) => {
+    console.log("trying to set the link");
+    const cardLink = activeLinkCard;
+    const cardToBeLinked = id;
 
-        </div>
-        </>
-        
-      ))
+    if (activeLinkCard) {
+      setOrganisedCards((prevCards) => {
+        return prevCards.map((card) => {
+          return card.map((card_) => {
+            if (card_.id === cardToBeLinked) {
+              console.log(`modify ${cardToBeLinked}'s prevQ value to  ${cardLink}`);
+              card_.previousQuestion.push(cardLink);
+            }
+            return card_
+          })
+        })
+      })
     }
-  </div>
-    ))}
-    </div> 
-    </Xwrapper>
- </body>
-</html>
-    )
+  };
+
+
+  const cardExists = (id) => {
+    try {
+      return newCards.some((card) => card.id === id);
+    } catch (error) {
+      console.log("error on id: " + id);
+    }
+  }
+
+  return (
+    <html>
+      <head>
+      </head>
+      <body>
+        <p></p>
+        <button onClick={() => console.log(newCards)}>show new cards</button>
+        <Xwrapper>
+          <div style={{ display: 'flex', height: '100vh', flexDirection: 'row' }}>
+            {organisedCards.map((card_, index) => (
+
+              <div key={card_.id} style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                {
+                  card_.map((card_, index) => (
+                    <>
+                      {(Number.isInteger(card_.previousQuestion) && cardExists(card_.previousQuestion)) ? <Xarrow start={(card_.previousQuestion).toString()} end={(card_.id).toString()} /> : Array.isArray(card_.previousQuestion) && card_.previousQuestion.map((cardId) => cardExists(cardId) && <Xarrow start={(cardId).toString()} end={(card_.id).toString()} />)}
+                      <div style={{ display: 'flex', justifyContent: 'space-evenly', width: '100%' }}>
+
+                        <Card handleCardClick={handleCardClick} editCardContent={editCardContent} id={card_.id} setActiveLinkCard={setActiveLinkCard} deleteCard={deleteCard} previousQuestionId={card_.previousQuestion} isPrevQ={idInPreviousQuestionIds(card_.id)} title={Object.hasOwn(card_, "question") ? "Question" : "Option"} layer={card_.layer} content={card_.text ? card_.text : card_.question} spawnNewCard={spawnNewCard} />
+
+                      </div>
+                    </>
+
+                  ))
+                }
+              </div>
+            ))}
+          </div>
+        </Xwrapper>
+      </body>
+    </html>
+  )
 }
 
 export default Testing
